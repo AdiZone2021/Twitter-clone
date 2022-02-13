33b40fdd27338093094d7637438efa1781fb6301
AdiZone) <86079921+AdiZone2021@users.noreply.github.com>
2022-02-14 02:16:12 +0600

    teiri

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKXw2zdW-PSnsr6GTNknXxhkcsSj_7vnA",
-  authDomain: "twitter-clone-adi.firebaseapp.com",
-  projectId: "twitter-clone-adi",
-  storageBucket: "twitter-clone-adi.appspot.com",
-  messagingSenderId: "637438922403",
-  appId: "1:637438922403:web:41e6c2d9f52737746b3916",
-  measurementId: "G-X9X41DJTER"
+  apiKey: "AIzaSyDQtn_Wb3M3v3e2iLOvBA9uA93yb-dP05Q",
+  authDomain: "twitter-29713.firebaseapp.com",
+  projectId: "twitter-29713",
+  storageBucket: "twitter-29713.appspot.com",
+  messagingSenderId: "962809528090",
+  appId: "1:962809528090:web:42601481a27b3d223bb6a3",
+  measurementId: "G-X0CM1XZ518"
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 
